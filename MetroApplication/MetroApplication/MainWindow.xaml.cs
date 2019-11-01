7fdebb870f4abe66d83f8e89cd96f66fc6d5c16d
Chris Wodarczyk <chris_wodarczyk@bio-rad.com>
2019-11-01 00:21:49 -0700

    Picking and installing MySqlConnection library and testing connection.

--- MetroApplication/MetroApplication/MainWindow.xaml.cs
+++ MetroApplication/MetroApplication/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace MetroApplication
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadedEventHandler(object sender, RoutedEventArgs e)
+        {
+            using var connnection = new MySqlConnection();
         }
     }
 }
